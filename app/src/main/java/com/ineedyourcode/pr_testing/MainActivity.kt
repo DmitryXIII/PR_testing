59324c98268563635b38c4ed97fc23a6f6d06f58
Dmitry Gusev <dmtrxiii@gmail.com>
2022-04-09 12:31:33 +0300

    branch 1 - comments added

--- app/src/main/java/com/ineedyourcode/pr_testing/MainActivity.kt
+++ app/src/main/java/com/ineedyourcode/pr_testing/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ветка 1
+        //ветка 1
+        //ветка 1
+        //ветка 1
+        //ветка 1
+        //ветка 1
+        //ветка 1
+        //ветка 1
     }
 }
